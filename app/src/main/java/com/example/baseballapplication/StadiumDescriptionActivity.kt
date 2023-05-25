2e0d5a2e4363e438027da4655064e98f29dde7e2
JanP2002 <268446@student.pwr.edu.pl>
2023-05-25 11:41:22 +0200

    Adds empty StadiumDescriptionActivity and TeamDescriptionActivity

---
+++ app/src/main/java/com/example/baseballapplication/StadiumDescriptionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.baseballapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StadiumDescriptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stadium_description)
+    }
+}

dc2ae9f3e3c530aeb0f01faabe9215290dc15228
JanP2002 <268446@student.pwr.edu.pl>
2023-05-26 12:51:38 +0200

    Corrects favourites players on fragments

--- app/src/main/java/com/example/baseballapplication/PlayerDescriptionActivity.kt
+++ app/src/main/java/com/example/baseballapplication/PlayerDescriptionActivity.kt
@@ -2,13 +2,19 @@ package com.example.baseballapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.squareup.picasso.Picasso
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.properties.Delegates
 
 class PlayerDescriptionActivity : AppCompatActivity() {
     var playerName = ""
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_description)
@@ -21,5 +27,6 @@ class PlayerDescriptionActivity : AppCompatActivity() {
         }
 
     }
+
 
 }

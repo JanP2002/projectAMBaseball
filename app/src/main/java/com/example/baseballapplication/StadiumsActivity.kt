23387f42b9990f1e6b700d48d7c1846b9ce2ebda
JanP2002 <268446@student.pwr.edu.pl>
2023-05-27 12:39:34 +0200

    Adds StadiumsFragment for portrait mode

--- app/src/main/java/com/example/baseballapplication/StadiumsActivity.kt
+++ app/src/main/java/com/example/baseballapplication/StadiumsActivity.kt
@@ -6,25 +6,14 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class StadiumsActivity : AppCompatActivity(),RecyclerViewInterface {
+class StadiumsActivity : AppCompatActivity() {
     var stadiums = ArrayList<StadiumModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stadiums)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerStadiums)
 
-        stadiums = intent.getParcelableArrayListExtra("stadiums")!!
-
-        val adapter = StadiumRecyclerViewAdapter(this,stadiums,this)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
         this.supportActionBar?.title = "Stadiony"
 
     }
 
-    override fun onItemClick(position: Int) {
-        val intent = Intent(this,StadiumDescriptionActivity::class.java )
-        intent.putExtra("stadium",stadiums[position])
-        startActivity(intent)
-    }
 }
---
+++ app/src/main/java/com/example/baseballapplication/StadiumsFragment.kt
@@ -0,0 +1,42 @@
+package com.example.baseballapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.baseballapplication.R
+
+
+
+
+class StadiumsFragment : Fragment(), RecyclerViewInterface {
+    var stadiums = ArrayList<StadiumModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_stadiums, container, false)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerStadiums)
+
+        stadiums = requireActivity().intent.getParcelableArrayListExtra("stadiums")!!
+
+        val adapter = StadiumRecyclerViewAdapter(requireContext(), stadiums,this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        return view
+    }
+
+    override fun onItemClick(position: Int) {
+        val intent = Intent(requireContext(), StadiumDescriptionActivity::class.java )
+        intent.putExtra("stadium",stadiums[position])
+        startActivity(intent)
+    }
+
+
+}
